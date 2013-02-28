c903b933de8b19f1cfbbdeb2ff5320e0b53cafa8
Sts350 <sgouglas@ualberta.ca>
2013-02-28 12:26:39 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharacterCreator::Application.config.secret_token = '72d11c423080d4a53ab9f7b01cffca91ea71ad95497e3efd51199d19a84bbe06be2627b4b9c834b55117601769fddf2cfc9d45446467d77b7b4e0205c34abdad'
---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "users/show" do
+  before(:each) do
+    @user = assign(:user, stub_model(User,
+      :username => "Username",
+      :password => "",
+      :email => "Email"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Username/)
+    rendered.should match(//)
+    rendered.should match(/Email/)
+  end
+end
